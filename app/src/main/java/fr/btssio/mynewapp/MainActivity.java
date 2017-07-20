57b5e3ae7f2ed90c0992f7052b4585642e821215
Mysh3ll <michel.pompas@greta-sud-aquitaine.fr>
2017-07-20 21:15:42 +0200

    onClick Button
    Gestion du clique sur un bouton

--- app/src/main/java/fr/btssio/mynewapp/MainActivity.java
+++ app/src/main/java/fr/btssio/mynewapp/MainActivity.java
@@ -2,6 +2,8 @@ package fr.btssio.mynewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //Récupération de la vue/présentation de notre activité
         setContentView(R.layout.activity_main);
+    }
+
+    //Appel de notre méthode "onClick" du bouton provenant de notre Layout
+    public void actionBouton (View v) {
+
+        //Récupération de notre élément TextView depuis notre Layout
+        TextView tv = (TextView) findViewById(R.id.message);
+
+        //Changement de la valeur de notre texte pour l'élément TextView
+        tv.setText("Le dev c'est cool !");
     }
 }
