a2699ed39ec419017eb19f816556d0531ebdba52
Mysh3ll <michel.pompas@greta-sud-aquitaine.fr>
2017-07-20 09:36:45 +0200

    Layout activity_main modified
    Quelques changements basiques de notre Layout

--- app/src/main/java/fr/btssio/mynewapp/MainActivity.java
+++ app/src/main/java/fr/btssio/mynewapp/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Méthode onCreate du cycle de vie de l'activité
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Récupération de la vue/présentation de notre activité
         setContentView(R.layout.activity_main);
     }
 }
