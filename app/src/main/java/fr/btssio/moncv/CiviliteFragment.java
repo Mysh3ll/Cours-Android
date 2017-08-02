8808b2ab695b0cb490ab939d17534890fe3af73f
Mysh3ll <michel.pompas@greta-sud-aquitaine.fr>
2017-08-02 14:04:40 +0200

    NavigationView + Fragment
    App MonCV

---
+++ app/src/main/java/fr/btssio/moncv/CiviliteFragment.java
@@ -0,0 +1,18 @@
+package fr.btssio.moncv;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CiviliteFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_civilite, container, false);
+    }
+
+}
--- app/src/main/java/fr/btssio/moncv/MainActivity.java
+++ app/src/main/java/fr/btssio/moncv/MainActivity.java
@@ -1,13 +1,114 @@
 package fr.btssio.moncv;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private boolean doubleBackToExitPressedOnce = false;
+    //Création du fragment
+    private Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Remplacement du fragment
+        fragment = new AboutFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.main_content, fragment);
+        ft.commit();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            if (doubleBackToExitPressedOnce) {
+                finish();
+            }
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Appuyer deux fois pour quitter", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 2000);
+        }
+    }
+
+    private void displaySelectedScreen(int itemId) {
+
+        //initialisation du fragemnt selon l'item
+        switch (itemId) {
+            case R.id.nav_civilite:
+                fragment = new CiviliteFragment();
+                break;
+            case R.id.nav_experience:
+//                fragment = new ExperienceFragment();
+                break;
+            case R.id.nav_formation:
+//                fragment = new FormationFragment();
+                break;
+            case R.id.nav_competence:
+//                fragment = new CompetenceFragment();
+                break;
+            case R.id.nav_langue:
+//                fragment = new LangueFragment();
+                break;
+            case R.id.nav_about:
+                fragment = new AboutFragment();
+                break;
+        }
+
+        //Remplacement du fragment
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.main_content, fragment);
+            ft.commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        //Appel de la méthode displayselectedscreen et on passe l'id du menu sélectionné
+        displaySelectedScreen(item.getItemId());
+        return true;
     }
 }
